66da1a93173255c0f3415838d52cd7a193196efc
20lillianjones <lej40@pitt.edu>
2024-12-03 10:11:20 -0500

    added query folder

--- Textual Tweet Training/Abortion/AbortionImageTraining.py
+++ Textual Tweet Training/Abortion/AbortionImageTraining.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 client = Client()
 
+"""
 #Read all of the image paths from the CSV file, along with the image stance:
 imagePaths=[]
 imageStance=[]
@@ -14,14 +15,15 @@ with open('Textual Tweet Training/Abortion/AbortionImageTrainingPaths.csv', 'r')
         imagePaths.append(tempRow[0])
         imageStance.append(tempRow[1])
 
-promptText="Here are some example images:\n"
+promptText="Here are some example images, as well as whether the image supports or opposes abortion:\n"
 
 for itNum in range(len(imagePaths)):
-    promptText+="Image #"+str(itNum+1)+": "+imagePaths[itNum]+"\n"
+    promptText+="Image #"+str(itNum+1)+": "+imagePaths[itNum]+" - "+imageStance[itNum]+"\n"
 
-promptText+="Describe Image #3. Be descriptive, and specify things are in the image. "
-
-print(promptText)
+promptText+="Based on the above examples, describe the following image, and determine if the following image supports or opposes abortion: "
+"""
+promptText="Describe the following image, and determine if the following image supports or opposes abortion: "
+promptImage="data/dataset/dataset/Abort/1332023865665581058.jpg"
 
 #Use Ollama to analyze the image with Llama 3.2-Vision:
 modelResponse = ollama.chat(
@@ -29,7 +31,7 @@ modelResponse = ollama.chat(
     messages=[{
       "role": "user",
       "content": promptText,
-      "images": [imagePaths[7]]
+      "images": [promptImage]
     }],
 )
 

8f9022f25aec9d09029e228a03a981ceac7e5f26
20lillianjones <lej40@pitt.edu>
2024-12-03 12:42:57 -0500

    guns

--- Textual Tweet Training/Gun Control/Image Processing/GunControlImageTraining.py
+++ Textual Tweet Training/Gun Control/Image Processing/GunControlImageTraining.py
@@ -7,7 +7,7 @@ client = Client()
 
 """
 This program uses prompt engineering coupled with the few-shot learning method. The
-model is asked to determine whether several images support or oppose gun control. No context is provided
+model is asked to determine whether several images support or oppose access to guns. No context is provided
 within the prompt. The accuracy is recorded. Then, the model is provided with context within the
 prompt (i.e. example images and their stance), and then asked to determine the stance of the same initial
 images. The accuracy is recorded, and compared to the initial results.
@@ -24,7 +24,7 @@ def interpretStance(llmOutput):
 #Collect the image paths, as well as the image stances, for the query set:
 queryPaths=[]
 queryStances=[]
-with open('Textual Tweet Training/Abortion/Image Processing/AbortionImageQueryPaths.csv', 'r') as file:
+with open('Textual Tweet Training/Gun Control/Image Processing/GunControlImageTrainingPaths.csv', 'r') as file:
     csvReader = csv.reader(file)
     for tempRow in csvReader:
         queryPaths.append(tempRow[0])
@@ -32,7 +32,7 @@ with open('Textual Tweet Training/Abortion/Image Processing/AbortionImageQueryPa
 
 #Without providing context, record the model's responses:
 print("Pre-Trained Model Results:")
-promptText="Determine if the following image supports access to abortion or opposes access to abortion. For each image, only respond with a single word: support, oppose"
+promptText="Determine if the following image supports access to guns or opposes access to guns. For each image, only respond with a single word: support, oppose"
 
 #Iterate through each query image, and find the result:
 responseAr=[]
@@ -65,19 +65,19 @@ responseAr=[]
 #Read all of the example image paths from the CSV file, along with the image stance:
 imagePaths=[]
 imageStance=[]
-with open('Textual Tweet Training/Abortion/Image Processing/AbortionImageTrainingPaths.csv', 'r') as file:
+with open('Textual Tweet Training/Abortion/Image Processing/GunControlImageQueryPaths.csv', 'r') as file:
     csvReader = csv.reader(file)
     for tempRow in csvReader:
         imagePaths.append(tempRow[0])
         imageStance.append(tempRow[1])
 
 #After providing context with few-shot learning, provide the model's response:
-promptText="Here are some example images, as well as whether the image supports or opposes abortion:\n"
+promptText="Here are some example images, as well as whether the image supports or opposes access to guns:\n"
 
 for itNum in range(len(imagePaths)):
     promptText+="Image #"+str(itNum+1)+": "+imagePaths[itNum]+" - "+imageStance[itNum]+"\n"
 
-promptText+="Based on the example images, determine if the following image supports access to abortion or opposes access to abortion. For each image, only respond with a single word: support, oppose"
+promptText+="Based on the example images, determine if the following image supports access to guns or opposes access to guns. For each image, only respond with a single word: support, oppose"
 
 #Use Ollama to analyze the image with Llama 3.2-Vision:
 for tempPath in queryPaths:
